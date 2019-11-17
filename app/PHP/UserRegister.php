d8d04df20b388c5d116d87905f312b0de8146e9e
Alex <52132138+par3k@users.noreply.github.com>
2019-11-17 14:21:07 +0800

    Update UserRegister.php

--- app/PHP/UserRegister.php
+++ app/PHP/UserRegister.php
@@ -1,19 +1,18 @@
 <?php
 
 $con = mysqli_connect ("localhost","bakhoijae","oexis7831","bakhoijae");
-//4����  ȸ�������� �Է¹޾����� 
+
 $userID = $_POST["userID"];
 $userPassword = $_POST["userPassword"];
 $userGender = $_POST["userGender"];
 $userEmail = $_POST["userEmail"];
 
-//User��� ��� ���̺� �ȿ� �ش� ������ �Է��Ҽ� �ֵ��� �Ѵ�.  
+$checkedPassword = password_hash($userPassword, PASSWORD_DEFAULT); // Encryption
 $statement = mysqli_prepare($con, "INSERT INTO M_USER VALUES (?, ?, ?, ?)");
-//������ ������ �� ���ڿ� �����̱⋚���� sssss�̴�. 
-mysqli_stmt_bind_param($statement, "ssss", $userID, $userPassword, $userGender, $userEmail);
+
+mysqli_stmt_bind_param($statement, "ssss", $userID, $checkedPassword, $userGender, $userEmail);
 mysqli_stmt_execute($statement);
 
-//ȸ������� �Ϸ�� ���Ŀ� success��� ������ true���� �־��־� ȸ������ ���� �� response�� �ؼ� ȸ�������� ���������� �˷��ش�.
 $response = array();
 $response["success"] = true;
 
