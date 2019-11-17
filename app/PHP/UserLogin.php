2684ae6f23623377fd954e754233a5986954ad6a
Alex <52132138+par3k@users.noreply.github.com>
2019-11-17 14:20:47 +0800

    Update UserLogin.php

--- app/PHP/UserLogin.php
+++ app/PHP/UserLogin.php
@@ -1,26 +1,25 @@
 <?php
 
 $con = mysqli_connect ("localhost","bakhoijae","oexis7831","bakhoijae");
-//4����  ȸ�������� �Է¹޾����� 
+
 $userID = $_POST["userID"];
 $userPassword = $_POST["userPassword"];
 
-//User��� ��� ���̺� �ȿ� �ش� ������ �Է��Ҽ� �ֵ��� �Ѵ�.  
-$statement = mysqli_prepare($con, "SELECT * FROM M_USER WHERE userID = ? AND userPassword = ?");
-mysqli_stmt_bind_param($statement, "ss", $userID, $userPassword);
+$statement = mysqli_prepare($con, "SELECT * FROM M_USER WHERE userID = ?");
+mysqli_stmt_bind_param($statement, "s", $userID);
 mysqli_stmt_execute($statement);
 
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $userID);
+mysqli_stmt_bind_result($statement, $userID, $checkedPassword, $userGender, $userEmail);
 
-//ȸ������� �Ϸ�� ���Ŀ� success��� ������ true���� �־��־� ȸ������ ���� �� response�� �ؼ� ȸ�������� ���������� �˷��ش�.
 $response = array();
 $response["success"] = false;
 
 while(mysqli_stmt_fetch($statement)){
-$response["success"] = true;
-$response["userID"] = $userID;
-
+    if(password_verify($userPassword, $checkedPassword)){
+        $response["success"] = true;
+        $response["userID"] = $userID;
+    }
 }
 
 echo json_encode($response);
